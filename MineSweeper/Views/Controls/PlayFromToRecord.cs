820598d66f361d92ee72ee867c3ff38585c02da3
markwardell <mark.d.wardell@gmail.com>
2025-04-21 20:01:15 -0500

    All in

--- MineSweeper/Views/Controls/PlayFromToRecord.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Maui.Graphics;
-
-namespace MineSweeper.Views.Controls;
-
-/// <summary>
-/// Record that contains two points representing a move from one position to another.
-/// Used for games like Chess or Checkers where pieces move from one position to another.
-/// </summary>
-public record PlayFromToRecord(Point FromPoint, Point ToPoint);
